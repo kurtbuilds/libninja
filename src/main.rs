84e16ffd9c82e1e710a79d7dad1ae8b91092ff22
Jonathan Lorimer <jonathan_lorimer@mac.com>
2022-09-21 10:14:43 -0400

    add lib and model opts

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,51 @@
 #![allow(non_snake_case)]
 #![allow(unused)]
 
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use anyhow::Result;
 use openapi_client_generator::{generate_library, generate_library_at_path, GenerateLibrary};
-use clap::{Command, Arg};
+use clap::{Parser, Subcommand};
 
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Adds files to myapp
+    Gen {
+        #[clap(short, long, value_parser)]
+        name: String,
+
+        #[clap(value_parser)]
+        spec: String,
+
+        #[clap(short, long, value_parser, default_value_t = String::from("src"))]
+        output_dir: String,
+
+        #[clap(short, long, value_parser)]
+        lib_rs: Option<String>,
+
+        #[clap(short, long, value_parser)]
+        model_rs: Option<String>,
+    },
+}
 
 fn main() -> Result<()> {
-    let matches = Command::new("openapi-client-generator")
-        .arg_required_else_help(true)
-        .subcommand_required(true)
-        .subcommand(Command::new("gen")
-            .arg(Arg::new("name")
-                .long("name")
-                .takes_value(true)
-                .required(true)
-            )
-            .arg(Arg::new("yaml_spec")
-                .required(true)
-            )
-            .arg(Arg::new("output_dir")
-                .long("output-dir")
-                .short('o')
-                .default_value("src")
-                .takes_value(true)
-            )
-        )
-        .get_matches();
-    match matches.subcommand().unwrap() {
-        ("gen", matches) => {
-            let name = matches.value_of("name").unwrap().to_string();
-            let yaml_spec = matches.value_of("yaml_spec").unwrap();
-            let output_dir = matches.value_of("output_dir").unwrap_or(&"src");
-
-            generate_library_at_path(Path::new(yaml_spec),GenerateLibrary {
-                name,
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Gen { name, spec, output_dir, lib_rs, model_rs } => {
+            generate_library_at_path(Path::new(spec),GenerateLibrary {
+                name: name.to_string(),
                 dest_path: output_dir.into(),
-                lib_rs_path: None,
-                model_rs_path: None,
+                lib_rs_path: lib_rs.as_ref().map(PathBuf::from),
+                model_rs_path: model_rs.as_ref().map(PathBuf::from),
             })?;
         }
-        _ => panic!("Unknown command"),
     }
     Ok(())
 }
